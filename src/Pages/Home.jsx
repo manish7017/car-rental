2e006572d2d5f7930db902470196de490a7e69e0
Manish Kumar <51908018+manish7017@users.noreply.github.com>
2023-09-23 19:03:25 +0530

    Update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,21 +6,19 @@ import Banner from "../components/Banner";
 import ChooseUs from "../components/ChooseUs";
 import Testimonials from "../components/Testimonials";
 import Faq from "../components/Faq";
-import Download from "../components/Download";
 import Footer from "../components/Footer";
 
 function Home() {
   return (
     <>
       <Hero />
-      <BookCar />
       <PlanTrip />
+      <BookCar />
       <PickCar />
       <Banner />
       <ChooseUs />
       <Testimonials />
       <Faq />
-      <Download />
       <Footer />
     </>
   );
